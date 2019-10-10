70919201e64e073ae97c3a20e17e1c35349bcfa3
Bernd Owsnicki-Klewe <bowsnickiklewe@uwf.edu>
2019-10-10 11:12:46 -0700

    JSON material added

--- workspace/JSONB/src/main/java/com/uwf/json1/JSONBindings.java
+++ workspace/JSONB/src/main/java/com/uwf/json1/JSONBindings.java
@@ -13,23 +13,41 @@ import javax.json.bind.JsonbBuilder;
  * @author bernd
  */
 public class JSONBindings {
-    
+
+    public static Employee createEmployee() {
+
+        Employee emp = new Employee();
+        emp.setId(100);
+        emp.setName("David");
+        emp.setPermanent(false);
+        emp.setPhoneNumbers(new long[]{1234567, 9876543});
+        emp.setRole("Manager");
+
+        Address add = new Address();
+        add.setCity("Baltimore");
+        add.setStreet("1234 ABC St");
+        add.setZipcode(21223);
+        emp.setAddress(add);
+
+        return emp;
+    }
+
     // Convert Employee instance to JSON String
     private static String emp2JSON(Employee emp) {
         Jsonb jsonb = JsonbBuilder.create();
         String jsonEmp = jsonb.toJson(emp);
         return jsonEmp;
     }
-    
+
     // Convert JSON String to Employee instance
     private static Employee json2EMP(String json) {
         Jsonb jsonb = JsonbBuilder.create();
         return jsonb.fromJson(json, Employee.class);
     }
-    
+
     // Demo!
     public static void main(String[] args) {
-        Employee emp = EmployeeJSONWriter.createEmployee();
+        Employee emp = createEmployee();
         String empJ = emp2JSON(emp);
         System.out.println(empJ);
         Employee emp2 = json2EMP(empJ);
